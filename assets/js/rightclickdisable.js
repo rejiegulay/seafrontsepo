2a2ad32f7d1ca4b46eb0b9bc83afe3e58eca68c6
seafrontsepo <gulayrejie@gmail.com>
2024-05-22 14:37:25 +0800

    Add files via upload

---
+++ assets/js/rightclickdisable.js
@@ -0,0 +1,10 @@
+<script type="text/javascript"> 
+function disableRightClick() 
+{ 
+alert("Sorry, right click is not allowed !!"); 
+return false; 
+} 
+</script> 
+<body oncontextmenu=" return disableRightClick();">
+... 
+</body>
---
+++ assets/mail/contact.php
@@ -0,0 +1,25 @@
+<?php
+if (isset($_POST['form-submit']))
+{
+    $to = "rejie.megabyte@gmail.com"; // Your email address
+    $name = $_POST['name'];
+    $from = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $subject = "Contact Form Submission";
+    $headers = "From:" . $from;
+    $result = mail($to, $subject, $message, $headers);
+
+    if ($result)
+    {
+        echo '<script type="text/javascript">alert("Your message was sent!");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+    }
+    else
+    {
+        echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+    }
+}
+?>
